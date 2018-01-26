ebe62e8d698f9e8752d2346969ec985d4d864a8f
goyalankit12 <goyalankit2012@gmail.com>
2018-01-25 22:27:57 -0700

    initial 6

--- index.php
+++ index.php
@@ -1211,11 +1211,11 @@
       </div>
     </footer>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap core JavaScript
+    <script src="vendor/jquery/jquery.min.js"></script>-->
+    <script src="js/bootstrap.bundle.min.js"></script>
     <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for this template -->
     <script src="js/new-age.min.js"></script>
